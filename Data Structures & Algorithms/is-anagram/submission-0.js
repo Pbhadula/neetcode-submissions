6e80ba6cc8838badfd12c8386163c6ca5622c80b
Prashant Bhadula <30542866+Pbhadula@users.noreply.github.com>
2026-09-17 22:32:55 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+
+        const count = new Array(26).fill(0);
+
+        for(let i=0; i<s.length; i++){
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+            count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+        }
+        console.log(count)
+
+        for(let val of count) {
+            if(val!=0) {
+            
+                return false
+            }
+        }
+        return true
+    }
+}
